0c552332317aba59bbd63f97ec346e85b82535c7
Manuel Ramirez <manuel2807.dev@gmail.com>
2025-03-13 12:03:44 -0500

    amigo secreto terminado y funcional

--- app.js
+++ app.js
@@ -1,2 +1,56 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let listaAmigos =[];
+
+let listaDeAmigos = [];
+let indiceGenerado = 99999;
+
+
+function agregarAmigo(){
+    let nombreAmigo = document.getElementById('amigo').value;
+    console.log(nombreAmigo);
+    if(nombreAmigo == ""){
+        alert("Por favor, inserte un nombre.")   
+    }else {
+        listaDeAmigos.push(nombreAmigo);
+        console.log(listaDeAmigos);
+        document.querySelector('#amigo').value='';
+        actualizaListaAmigos();
+    }
+}
+
+function actualizaListaAmigos(){
+    //listaAmigos
+    let elementoHTML = document.querySelector('#listaAmigos');
+        elementoHTML.innerHTML = "";
+
+        for (let i=0; i< listaDeAmigos.length; i++){
+            let li =document.createElement("li");
+            li.textContent = listaDeAmigos[i];
+            document.getElementById("listaAmigos").appendChild(li);
+        }
+}
+
+function sortearAmigo(){
+    if(listaDeAmigos.length>0){
+        console.log("hay uno o mas de uno");
+
+        generaIndice();
+        console.log(listaDeAmigos[indiceGenerado]);
+        visualizaGanador();
+
+    } else {
+        alert("La lista esta vacia, por favor ingrese nombres de amigos");
+    }
+    
+}
+
+    function  generaIndice(){
+        console.log("primero " + indiceGenerado);
+        while (indiceGenerado > listaDeAmigos.length){
+            indiceGenerado = Math.floor(Math.random()*10);
+            console.log(indiceGenerado);
+        }
+}
+function visualizaGanador(){
+    let elementoHTML = document.querySelector('#resultado');
+    elementoHTML.innerHTML = listaDeAmigos[indiceGenerado];
+}
